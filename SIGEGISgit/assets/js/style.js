0f98d799c7775b1f0a796585b7c3f0da0b5bcffe
Amadeus <darty@One-Two>
2012-08-24 11:54:50 +0000

    C'est fait !

---
+++ SIGEGISgit/assets/js/style.js
@@ -0,0 +1,41 @@
+$("input[id=valider]").button();
+$("#myRadio").buttonset();
+
+$('#menu-css li a').hover(   
+		  
+        function() {   
+  
+            $(this).css('padding', '5px 15px')   
+         .stop()
+         .animate({'backgroundColor':'rgba(0,0,0,0.5)'},   
+                     'fast');   
+         /*.animate({'paddingLeft'    : '17px',   
+                     'paddingRight' : '17px',   
+                     'backgroundColor':'rgba(0,0,0,0.5)'},   
+                     'fast');*/   
+        },   
+  
+        function() {   
+  
+            $(this).css('padding', '5px 15px')   
+         .stop()   
+         .animate({'paddingLeft'    : '15px',   
+                    'paddingRight'      : '15px',   
+                    'backgroundColor' :'rgba(0,0,0,0.2)'},   
+                    'fast');   
+  
+    }).mousedown(function() {   
+  
+        $(this).stop().animate({'backgroundColor': 'rgba(0,0,0,0.1)'}, 'fast');   
+  
+    }).mouseup(function() {   
+  
+        $(this).stop().animate({'backgroundColor': 'rgba(0,0,0,0.5)'}, 'fast');   
+    });   
+  
+	$("#zone_des_filtres").addClass("ui-state-default ui-corner-all");
+	$(".zone_des_options").addClass("ui-state-default ui-corner-all");
+	
+	$("#chartdiv2").hide();
+
+	$("#accordion").accordion({ header: "h3" });
